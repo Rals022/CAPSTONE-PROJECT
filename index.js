30168ce92d719fc76357f8da65a413e459ebae0d
Rals022 <raleighstevens7@gmail.com>
2023-05-07 16:33:26 -0400

    Capstone api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import axios from "axios";
 
 const router = new Navigo("/");
 
@@ -25,17 +26,54 @@ function afterRender(state) {
   });
 }
 
-router
-  .on({
-    "/": () => render(),
-    ":view": params => {
-      let view = capitalize(params.data.view);
-      // eslint-disable-next-line no-prototype-builtins
-      if (store.hasOwnProperty(view)) {
-        render(store[view]);
-      } else {
-        console.log(`View ${view} not defined`);
-      }
-    }
-  })
-  .resolve();
+Promise.all([
+  axios.get(
+    `https://www.mapquestapi.com/staticmap/v5/map?key=${process.env.MAP_QUEST_API}&center=Gorham,ME&size=600,400@2x
+    ,`
+  ),
+  // axios.post(`${process.env.MAP_QUEST_API}/map`, requestData),
+])
+
+  .then((response) => {
+    console.log(response);
+
+    }, );
+
+
+
+
+  // .catch((error) => {
+  //   console.log("It puked", error);
+  // });
+
+if (state.view === "Contact") {
+/*
+Please refer to the documentation:
+https://developer.mapquest.com/documentation/mapquest-js/v1.3/
+*/
+
+L.mapquest.key = process.env.MAP_QUEST_API;
+
+let map = L.mapquest.map("Contact",{
+  // street: '200 Narragansett Street',
+  // city: 'Gorham',
+  // state: 'ME',
+  // postalCode: '04038'
+  center: [42.361145, -71.057083],
+      layers: L.mapquest.tileLayer("map"),
+      zoom: 11,
+      zoomControl: true,
+});
+
+
+
+
+// map.addControl(L.mapquest.satelliteControl());
+map.addControl(L.mapquest.control());
+{
+map.addControl(
+L.mapquest.geocodingControl({
+  position: "topleft",
+})
+)};
+
