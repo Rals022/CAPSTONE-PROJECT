199448ed8095f77b14c81a4ee6a8cda61e0679e8
Rals022 <raleighstevens7@gmail.com>
2023-05-28 11:01:08 -0400

    added aboutme,home,css

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function render(state = store.Home) {
       console.log("request Body", requestData);
 
       axios
-        .post(`https://myspa-survey2023.onrender.com/survey`, requestData)
+        .post(`https://myspa-survey2023.onrender.com/Survey`, requestData)
         .then(response => {
           // Push the new survey onto the Survey state survey attribute, so it can be displayed in the survey graph and list
           store.Survey.surveys.push(response.data);
